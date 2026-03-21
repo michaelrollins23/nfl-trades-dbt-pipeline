085a95c2a49b36eebbc8c6fdf9508caab6e32b2f
Michael Rollins <michaelwrollins@gmail.com>
2026-03-21 19:12:03 +0000

    completed stg_seatgeek_events.sql

--- models/staging/seatgeek/stg_seatgeek_events.sql
+++ models/staging/seatgeek/stg_seatgeek_events.sql
@@ -4,5 +4,16 @@ WITH raw_events AS
     FROM {{source('seatgeek_raw_data','event_details')}}
 )
 
+, final AS
+(
+    SELECT event_id
+        , event_name
+        , event_venue
+        , event_type
+        -- Casting to DATE for ease to filter by year/month later
+        , CAST(event_date AS date) AS event_date
+    FROM raw_events
+)
+
 SELECT *
-FROM raw_events
+FROM final
